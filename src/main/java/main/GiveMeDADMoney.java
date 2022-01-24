72d344df949917a20ab64430cc7b62fa47feef6d
Pablo <47283527+PabloSR06@users.noreply.github.com>
2022-01-24 13:30:08 +0000

    alertas

--- src/main/java/main/GiveMeDADMoney.java
+++ src/main/java/main/GiveMeDADMoney.java
@@ -2,6 +2,9 @@ package main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import menuController.MenuRootController;
 
@@ -22,6 +25,33 @@ public class GiveMeDADMoney extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
+	}
+	
+	public static void error(String header, String content) {
+		Alert error = new Alert(AlertType.ERROR);
+		error.initOwner(getPrimaryStage());
+		error.setTitle("Error");
+		error.setHeaderText(header);
+		error.setContentText(content);
+		error.showAndWait();
+	}
+	
+	public static boolean confirm(String title, String header, String content) {
+		Alert confirm = new Alert(AlertType.CONFIRMATION);
+		confirm.initOwner(getPrimaryStage());
+		confirm.setTitle(title);
+		confirm.setHeaderText(header);
+		confirm.setContentText(content);
+		return confirm.showAndWait().get().equals(ButtonType.OK);
+	}
+	
+	public static void info(String header, String content) {
+		Alert info = new Alert(AlertType.INFORMATION);
+		info.initOwner(getPrimaryStage());
+		info.setTitle(getPrimaryStage().getTitle());
+		info.setHeaderText(header);
+		info.setContentText(content);
+		info.showAndWait();
 	}
 
 	public static void main(String[] args) {
