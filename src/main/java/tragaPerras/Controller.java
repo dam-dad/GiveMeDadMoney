edb0e427c6f637836fb93af8d90273955611bb44
DAlexTF <dags92@gmail.com>
2022-01-31 19:08:02 +0000

    Update Controller.java

--- src/main/java/tragaPerras/Controller.java
+++ src/main/java/tragaPerras/Controller.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -57,7 +58,10 @@ public class Controller implements Initializable {
     private ImageView imageBlock1, imageBlock2, imageBlock3;
 
     @FXML
-    private Text puntosText, apuestaText;
+    private Text puntosText;
+
+    @FXML
+    private TextField apuestaText;
 
     @FXML
     private Text resultText;
