1f5aa6b94735784804398537dce30e6ff51d01a3
Diego04012002 <dgarciadeus@gmail.com>
2022-02-01 14:01:15 +0000

    sonidos

--- src/main/java/menuController/BaseController.java
+++ src/main/java/menuController/BaseController.java
@@ -1,36 +1,36 @@
 package menuController;
 
-
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import main.GiveMeDADMoney;
 import main.Sounds;
 import mayorOMenor.MayorOMenorController;
 import tragaPerras.Controller;
-
 
 public class BaseController implements Initializable {
 
 	@FXML
 	private BorderPane root;
 
-	//CONTROLLER
+	// CONTROLLER
 	private SettingsController settingController = new SettingsController();
 	private MenuController menuController = new MenuController();
 
 	private static BaseController instance;
 
-
-	
-	//CONTROLLER JUEGO
+	// CONTROLLER JUEGO
 	private Controller tragaPerrasController = new Controller();
 	private MayorOMenorController mayorOMenor = new MayorOMenorController();
-	
-	public Sounds musica;
+
+	// public Sounds musica;
 
 	private BaseController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Menu/BaseView.fxml"));
@@ -42,30 +42,38 @@ public class BaseController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		getView().getStylesheets().add("/css/Menu/menu.css");
 		showMenu();
-		
-		
-		try {
-			musica = new Sounds("sound/sound2.wav");
-		} catch (Exception e) {
 
-		}
-		
-		musica.play();
-		musica.volumen(-25.0F);
-		
+		backgroundMusic();
 		root.getStylesheets().add("");
+	}
+
+	Media musica;
+	MediaPlayer reproductor;
+
+	public void backgroundMusic() {
+		// Musica de fondo
+		try {
+			musica = new Media(GiveMeDADMoney.class.getResource("/media/sound2.mp3").toURI().toString());
+			reproductor = new MediaPlayer(musica);
+			reproductor.setAutoPlay(true);
+			reproductor.setCycleCount(MediaPlayer.INDEFINITE);
+
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void showSetting() {
 		root.setCenter(settingController.getView());
 
 	}
-	
+
 	public void showTragaPerras() {
 		root.setCenter(tragaPerrasController.getView());
 		tragaPerrasController.load_score();
-		
+
 	}
+
 	public void showMayorOMenor() {
 		root.setCenter(mayorOMenor.getView());
 
@@ -90,7 +98,5 @@ public class BaseController implements Initializable {
 		}
 		return instance;
 	}
-
-
 
 }
