79ef7bfecf4be34939b8d619ddfdb75356a94eb9
Diego04012002 <dgarciadeus@gmail.com>
2022-02-09 09:34:13 +0000

    cambios cuboController

--- src/main/java/cubeTower/cubeTowerController.java
+++ src/main/java/cubeTower/cubeTowerController.java
@@ -3,33 +3,26 @@ package cubeTower;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.TimeUnit;
-
-import com.sun.javafx.perf.PerformanceTracker;
 
 import javafx.animation.AnimationTimer;
-import javafx.animation.Interpolator;
-import javafx.animation.Transition;
-import javafx.animation.TranslateTransition;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.util.Duration;
 import menuController.BaseController;
 
 public class cubeTowerController extends AnimationTimer implements Initializable {
 
 	private long time = 0L, last;
-	private int posX = 0, posY = 4;
+	private int posX = 0, posY = 4, tamanoConColor = 0;
 	private boolean choque = false;
-	private boolean inicio = true, termino = false;
+	private boolean inicio = true, termino;
 
 	private Pane[][] rectangulos;
 
@@ -62,7 +55,8 @@ public class cubeTowerController extends AnimationTimer implements Initializable
 		}
 		setColor(posX, posY);
 		setColor(posX + 1, posY);
-
+		tamanoConColor = 2;
+		inicio = true;
 	}
 
 	public BorderPane getView() {
@@ -87,18 +81,31 @@ public class cubeTowerController extends AnimationTimer implements Initializable
 
 		//
 		if (inicio == false) {
-			setTransparent(posX, posY);
-			setTransparent(posX + 1, posY);
+			for (int i = 0; i < 2; i++) {
+				setTransparent(posX + i, posY);
+			}
+		}
+		if (inicio == true) {
+			for (int i = 0; i < tamanoConColor; i++) {
+				setTransparent(posX + i, posY);
+			}
 		}
 		// update
 		if (time > 0.5 * 1e9 && termino == false) {
 			bloqueDeMovimiento2();
 			time -= 0.5 * 1e9;
-		} else {
-			setColor(posX, posY);
-			setColor(posX + 1, posY);
-			inicio = false;
 		}
+		if (inicio == false) {
+			for (int i = 0; i < 2; i++) {
+				setColor(posX + i, posY);
+			}
+		}
+		if (inicio == true) {
+			for (int i = 0; i < tamanoConColor; i++) {
+				setColor(posX + i, posY);
+			}
+		}
+
 	}
 
 	private void bloqueDeMovimiento2() {
@@ -124,72 +131,79 @@ public class cubeTowerController extends AnimationTimer implements Initializable
 
 	@FXML
 	void onStopAction(ActionEvent event) {
+		tamanoConColor = 0;
 		stop();
-
 		if (posY != pixeles.getRowCount() - 1) {
-			System.out.println("hola");
-			if (rectangulos[posX][posY].getStyle() != rectangulos[posX][posY + 1].getStyle()
-					&& rectangulos[posX + 1][posY].getStyle() != rectangulos[posX + 1][posY + 1].getStyle()) {
-				stop();
-				setTransparent(posX, posY);
-				setTransparent(posX+1, posY);
-				Alert alertaTope = new Alert(AlertType.INFORMATION);
-				alertaTope.setHeaderText("Has perdido");
-				alertaTope.setContentText("¿Reiniciar?");
-				alertaTope.showAndWait();
-				termino=true;
-			} else {
-				if (rectangulos[posX][posY].getStyle() != rectangulos[posX][posY + 1].getStyle() && choque == false) {
-					System.out.println("pasa derecha");
-					rectangulos[posX][posY].setStyle("-fx-background-color:transparent");
+			for (int i = 0; i < pixeles.getColumnCount(); i++) {
+				if (rectangulos[i][posY].getStyle() != "-fx-background-color:transparent") {
+					if (rectangulos[i][posY].getStyle() != rectangulos[i][posY + 1].getStyle()) {
+						rectangulos[i][posY].setStyle("-fx-background-color:transparent");
+					} else {
+						tamanoConColor++;
+					}
 				}
-				if (rectangulos[posX + 1][posY].getStyle() != rectangulos[posX + 1][posY + 1].getStyle()
-						&& choque == false) {
-					System.out.println("pasa derecha 2");
-					rectangulos[posX + 1][posY].setStyle("-fx-background-color:transparent");
-				}
-				System.out.println(posX + " " + posY);
-				if (rectangulos[posX][posY].getStyle() != rectangulos[posX][posY + 1].getStyle() && choque == true) {
-					System.out.println("pasa derecha");
-					rectangulos[posX][posY].setStyle("-fx-background-color:transparent");
-				}
-				if (rectangulos[posX + 1][posY].getStyle() != rectangulos[posX + 1][posY + 1].getStyle()
-						&& choque == true) {
-					System.out.println("pasa derecha 2");
-					rectangulos[posX + 1][posY].setStyle("-fx-background-color:transparent");
-				}
-//				if (rectangulos[posX+1][posY].getStyle() != rectangulos[posX][posY + 1].getStyle()
-//						&& choque == true) {
-//					rectangulos[posX-1][posY].setStyle("-fx-background-color:transparent");
-//					System.out.println("pasa 2 izquierda");
-//				}
-//				if (rectangulos[posX][posY].getStyle() != rectangulos[posX][posY + 1].getStyle()
-//						&& choque == false) {
-//					System.out.println("pasa dercha");
+			}
+		}
+		if (posY == 0) {
+			stop();
+		} else {
+			System.out.println(tamanoConColor);
+			System.out.println(posX + " " + posY);
+			posY--;
+			posX = 0;
+			start();
+		}
+		if (tamanoConColor == 0) {
+			inicio = false;
+		}
+
+//		if (posY != pixeles.getRowCount() - 1) {
+//			System.out.println("hola");
+//			if (rectangulos[posX][posY].getStyle() != rectangulos[posX][posY + 1].getStyle()
+//					&& rectangulos[posX + 1][posY].getStyle() != rectangulos[posX + 1][posY + 1].getStyle()) {
+//				stop();
+//				setTransparent(posX, posY);
+//				setTransparent(posX + 1, posY);
+//				Alert alertaTope = new Alert(AlertType.INFORMATION);
+//				alertaTope.setHeaderText("Has perdido");
+//				alertaTope.setContentText("¿Reiniciar?");
+//				alertaTope.showAndWait();
+//				termino = true;
+//			} else {
+//				if (rectangulos[posX][posY].getStyle() != rectangulos[posX][posY + 1].getStyle() && choque == false) {
+//					System.out.println("pasa derecha");
 //					rectangulos[posX][posY].setStyle("-fx-background-color:transparent");
 //				}
 //				if (rectangulos[posX + 1][posY].getStyle() != rectangulos[posX + 1][posY + 1].getStyle()
 //						&& choque == false) {
-//					rectangulos[posX+1][posY].setStyle("-fx-background-color:transparent");
-//					System.out.println("pasa2 derecha");
+//					System.out.println("pasa derecha 2");
+//					rectangulos[posX + 1][posY].setStyle("-fx-background-color:transparent");
 //				}
-			}
-//			rectangulos[posX][posY].setStyle("-fx-background-color:transparent");
-//			rectangulos[posX+1][posY].setStyle("-fx-background-color:transparent");
-			posX = 1;
-			posY--;
-		} else {
-			posY--;
-		}
-		if (posY < 0) {
-			stop();
-			Alert alertaTope = new Alert(AlertType.INFORMATION);
-			alertaTope.setHeaderText("Completado");
-			alertaTope.setContentText("Has completado el nivel");
-			alertaTope.showAndWait();
-		} else {
-			start();
-		}
+//				System.out.println(posX + " " + posY);
+//				if (rectangulos[posX][posY].getStyle() != rectangulos[posX][posY + 1].getStyle() && choque == true) {
+//					System.out.println("pasa derecha");
+//					rectangulos[posX][posY].setStyle("-fx-background-color:transparent");
+//				}
+//				if (rectangulos[posX + 1][posY].getStyle() != rectangulos[posX + 1][posY + 1].getStyle()
+//						&& choque == true) {
+//					System.out.println("pasa derecha 2");
+//					rectangulos[posX + 1][posY].setStyle("-fx-background-color:transparent");
+//				}
+//			}
+//			posX = 1;
+//			posY--;
+//		} else {
+//			posY--;
+//		}
+//		if (posY < 0) {
+//			stop();
+//			Alert alertaTope = new Alert(AlertType.INFORMATION);
+//			alertaTope.setHeaderText("Completado");
+//			alertaTope.setContentText("Has completado el nivel");
+//			alertaTope.showAndWait();
+//		} else {
+//			start();
+//		}
 	}
 
 	@FXML
