5de211d1018308ad3de32344cd37269e3d924cb9
Pablo <47283527+PabloSR06@users.noreply.github.com>
2022-02-15 09:57:46 +0000

    Acceder a la tienda

--- src/main/java/store/StoreController.java
+++ src/main/java/store/StoreController.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
 
 public class StoreController implements Initializable {
 	
+    @FXML
+    private BorderPane root;
+
 	
 	public StoreController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Store/Store.fxml"));
@@ -19,6 +24,11 @@ public class StoreController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources)  {
 		
+	}
+
+	
+	public BorderPane getView() {
+		return root;
 	}
 
 }
