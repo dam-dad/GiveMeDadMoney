09bb472509ed91f1ce5e67b8a9aad9bc51d75db1
Pablo <47283527+PabloSR06@users.noreply.github.com>
2022-02-18 09:16:27 +0000

    pruebas gif

--- src/main/java/store/StoreController.java
+++ src/main/java/store/StoreController.java
@@ -4,17 +4,45 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class StoreController implements Initializable {
-	
-    @FXML
-    private BorderPane root;
 
-	
+	@FXML
+	private ImageView ima;
+
+	@FXML
+	private Button prueba;
+
+	@FXML
+	private Button reward1Button;
+
+	@FXML
+	private VBox rewardVBox;
+
+	@FXML
+	private BorderPane root;
+
+	@FXML
+	void onReward1Action(ActionEvent event) {
+		rewardVBox.setVisible(true);
+	}
+
+	@FXML
+	void prueba(ActionEvent event) {
+		ima.setImage(new Image("/images/giphy.gif"));
+		
+		
+	}
+
 	public StoreController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Store/Store.fxml"));
 		loader.setController(this);
@@ -22,11 +50,10 @@ public class StoreController implements Initializable {
 	}
 
 	@Override
-	public void initialize(URL location, ResourceBundle resources)  {
-		
+	public void initialize(URL location, ResourceBundle resources) {
+
 	}
 
-	
 	public BorderPane getView() {
 		return root;
 	}
